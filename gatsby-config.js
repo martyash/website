56b6ac8f51c4cdca17d2d26bc233bd48708acc97
Murtaza <m.ashraf@unswalumni.com>
2022-07-14 20:12:56 +1000

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter template`,
+    title: `Murtaza Ashraf`,
   },
+  flags: {
+
+    PRESERVE_WEBPACK_CACHE: true,
+
+    PRESERVE_FILE_DOWNLOAD_CACHE: true,
+
+    FAST_DEV: true,
+
+    FAST_REFRESH: true,
+
+  },
+
   plugins: [],
 }
